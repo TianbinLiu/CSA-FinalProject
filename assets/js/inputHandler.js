4c3a9e14f0a25aeb0d462edd3c34b05df423df00
TianbinLiu <89219591+TianbinLiu@users.noreply.github.com>
2023-05-15 00:20:27 -0700

    Update inputHandler.js

--- assets/js/inputHandler.js
+++ assets/js/inputHandler.js
@@ -1,13 +1,15 @@
-addEventListener("keydown", function(e){
-    if (e.code == 'keyD') vxr = 5;
-    if (e.code == 'keyA') vxl = -5;
-    if (e.code == 'keyS') vy = 5;
-    if (e.code == 'keyW') vy = -5;
+console.log("inputHandler.js loaded")
+
+addEventListener("keydown", function(event){
+    if (event.code == 'ArrowRight') vxr = 5;
+    if (event.code == 'ArrowLeft') vxl = -5;
+    if (event.code == 'ArrowDown') vy = 5;
+    if (event.code == 'ArrowUp') vy = -5;
 })
 
-addEventListener("keyup", function(e){
-    if (e.code == 'keyD') vxr = 0;
-    if (e.code == 'keyD') vxl = 0;
-    if (e.code == 'keyD') vx = 0;
-    if (e.code == 'keyD') vx = 0;
+addEventListener("keyup", function(event){
+    if (event.code == 'ArrowRight') vxr = 0;
+    if (event.code == 'ArrowLeft') vxl = 0;
+    if (event.code == 'ArrowDown') vy = 0;
+    if (event.code == 'ArrowUp') vy = 0;
 })
