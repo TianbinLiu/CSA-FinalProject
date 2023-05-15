d9378cc3a09512261c739bbf030491b6d5475450
TianbinLiu <l196521542@gmail.com>
2023-05-15 01:15:12 -0700

    1

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,26 +1,13 @@
 console.log("Main.js loaded")
 
-function resize() {
 const canvas = document.getElementById("canvas")
-var canvasRatio = canvas.height / canvas.width;
-var windowRatio = window.innerHeight / window.innerWidth;
-var width;
-var height;
-
-if (windowRatio < canvasRatio) {
-    height = window.innerHeight;
-    width = height / canvasRatio;
-} else {
-    width = window.innerWidth;
-    height = width * canvasRatio;
-}
-
-canvas.style.width = width + 'px';
-canvas.style.height = height + 'px';
-}
-window.addEventListener('resize', resize, false);
-
 const ctx = canvas.getContext("2d")
+const observer = new ResizeObserver((entries) => {
+    canvas.width = canvas.clientWidth;
+    canvas.height = canvas.clientHeight;
+});
+observer.observe(canvas)
+
 
 let x = 0;
 let y = 0;
