65b66ea266be3756a01d1a18ee7156d98aeb758f
TianbinLiu <l196521542@gmail.com>
2023-05-17 01:14:26 -0700

    1

--- assets/js/GameObject.js
+++ assets/js/GameObject.js
@@ -2,7 +2,7 @@ class GameObject {
   constructor(config) {
     this.x = config.x || 0;
     this.y = config.y || 0;
-    this.direction = direction || "down";
+    this.direction = direction || "right";
     this.sprite = new Sprite({
       gameObject: this,
       src: config.src || "https://tianbinliu.github.io/CSA-FinalProject/images/character/adventurer-v1.5-Sheetflip.png",
