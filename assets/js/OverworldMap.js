88945ca8c967d859a9c1bc1a5e941bb494cb7c7f
TianbinLiu <l196521542@gmail.com>
2023-05-24 00:43:08 -0700

    1

--- assets/js/OverworldMap.js
+++ assets/js/OverworldMap.js
@@ -116,7 +116,7 @@ window.OverworldMaps = {
           { type: "stand",  direction: "right", time: 1200 },
         ]
       }),
-      Wizard: new GameObject({
+      Wizard: new Person({
         isMounted: true,
         x: utils.withGrid(3),
         y: utils.withGrid(7),
@@ -124,6 +124,10 @@ window.OverworldMaps = {
         sizey: 70,
         id: "Wizard",
         src: "https://tianbinliu.github.io/CSA-FinalProject/images/character/wizard/WizardMrM.png",
+        behaviorLoop: [
+          { type: "stand",  direction: "left", time: 800 },
+          { type: "stand",  direction: "right", time: 1200 },
+        ]
       }),
     },
     walls: {
