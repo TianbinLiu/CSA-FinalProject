8f9777f2096ae67821035da3eb374a1d05c18dac
TianbinLiu <l196521542@gmail.com>
2023-05-23 23:22:39 -0700

    1

--- assets/js/OverworldMap.js
+++ assets/js/OverworldMap.js
@@ -119,11 +119,8 @@ window.OverworldMaps = {
         id: "Wizard",
         src: "https://tianbinliu.github.io/CSA-FinalProject/images/character/wizard/WizardMrM.png",
         behaviorLoop: [
-          { type: "walk",  direction: "left" ,spritedirection: "left"},
           { type: "stand",  direction: "left", time: 800 },
-          { type: "walk",  direction: "up" , spritedirection: "left"},
-          { type: "walk",  direction: "right", spritedirection: "right" },
-          { type: "walk",  direction: "down", spritedirection: "right" },
+          { type: "stand",  direction: "right", time: 1200 },
         ]
       }),
     },
