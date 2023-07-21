c7afcb8d0f09d14c412eb6959b3af409f36eeba0
TianbinLiu <89219591+TianbinLiu@users.noreply.github.com>
2023-07-21 18:14:33 +0800

    Update pio.js

--- assets/js/pio.js
+++ assets/js/pio.js
@@ -304,44 +304,5 @@ var Paul_Pio = function (prop) {
 
     localStorage.getItem("posterGirl") == 0 ? this.initHidden() : this.init();
 
-    const userInputElement = document.getElementById("user-input");
-    const submitButton = document.getElementById("submit-btn");
-
-    let userInputArray = [];
-
-    function appendResponse(response) {
-      modules.render(response);
-    }
-
-    function handleUserInput() {
-      const userInput = userInputElement.value;
-      userInputArray.push(userInput);
-      userInputElement.value = "";
-
-      fetch("https://chatgpttesting.lol/api/chat", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          input: userInput,
-        }),
-      })
-        .then(response => response.json())
-        .then(data => {
-          appendResponse(data.output);
-        })
-        .catch(error => {
-          console.error("Error:", error);
-        });
-    }
-
-    submitButton.addEventListener("click", handleUserInput);
-    userInputElement.addEventListener("keydown", function (event) {
-      if (event.key === "Enter") {
-        handleUserInput();
-      }
-    });
-
 };
 
